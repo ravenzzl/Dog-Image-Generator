f745dfc78abc279218921991f4a728c564f0c9da
ravenzzl <ravenzzco1@gmail.com>
2025-06-05 11:29:18 +0530

    base project

---
+++ src/main/java/com/dogImg/DogImgGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.dogImg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogImgGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogImgGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dogImg/DogImgGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dogImg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogImgGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
